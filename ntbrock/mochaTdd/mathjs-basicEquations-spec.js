05bc63f3aef237333f5b9c5385f485c560c5d8ae
rsteed11 <rsteed11@gmail.com>
2016-06-15 13:11:13 -0400

    Changed layout of webpage

--- ntbrock/mochaTdd/mathjs-basicEquations-spec.js
+++ ntbrock/mochaTdd/mathjs-basicEquations-spec.js
@@ -6,7 +6,8 @@ var expect=require('chai').expect;
 var math = require('mathjs');
 
 describe('mathjs-basicEquations-require', function() {
-	it('should add 1+2', function() {
+	it('should add 1+2', fun
+ction() {
 
 		var eqn = math.compile("1+2");
 		expect(eqn.eval()).to.equal(3);
