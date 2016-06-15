d9abbd85aa0dd6a9b910cfaf9cdf0fb853ebb9c9
Taylor Brockman <taylor.brockman@gmail.com>
2016-06-14 20:46:58 -0400

    Sample vurlnerable script

---
+++ ntbrock/xmlToJsonPhp/xtoj.php
@@ -0,0 +1,21 @@
+<?php
+// http://stackoverflow.com/questions/8830599/php-convert-xml-to-json
+// 2016-Jun-14 Brockman concept for converting xml to json for iwp files
+
+$basePath = "../pps/etc/problems";
+$pathInfo = $_SERVER['PATH_INFO'];
+$fullPath = $basePath . $pathInfo;
+
+// echo $fullPath;
+
+$xml_string = file_get_contents($fullPath);
+
+$xml = simplexml_load_string($xml_string);
+$json = json_encode($xml);
+
+header('Content-Type: application/json');
+echo $json;
+
+$array = json_decode($json,TRUE);
+
+?>
